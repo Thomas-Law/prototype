e184927ab04a6cbded2d152a73de5fbb54099684
Thomas.KC.Law <Thomas.KC.Law@pccw.com>
2020-06-12 17:00:24 +0800

    mybatis

--- src/main/java/com/example/prototype/model/OrderLineItem.java
+++ src/main/java/com/example/prototype/model/OrderLineItem.java
@@ -3,6 +3,7 @@ package com.example.prototype.model;
 public class OrderLineItem {
 	
 	private String fulfillmentState;
+	private String paymentState;
 
 	public String getFulfillmentState() {
 		return fulfillmentState;
@@ -10,6 +11,14 @@ public class OrderLineItem {
 
 	public void setFulfillmentState(String fulfillmentState) {
 		this.fulfillmentState = fulfillmentState;
+	}
+
+	public String getPaymentState() {
+		return paymentState;
+	}
+
+	public void setPaymentState(String paymentState) {
+		this.paymentState = paymentState;
 	}
 
 }
